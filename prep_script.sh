5026f097049fe36a94f03c5fbe7a2b3773396f7f
GMoncrieff <glennwithtwons@gmail.com>
2018-05-15 19:22:54 +0200

    initialize

--- prep_script.sh
+++ prep_script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export PATH=/opt/anaconda/bin:$PATH
-conda create --yes --name ds python=3.5 
+conda create --yes --name ds python=3.5
+export PATH=/opt/anaconda/bin:$PATH 
 source activate ds
 jupyter notebook --generate-config
 echo "c = get_config()" >> ~/.jupyter/jupyter_notebook_config.py
