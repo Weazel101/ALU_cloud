174ee361e8a5ad57db4c8302816f50da56075f19
GMoncrieff <glennwithtwons@gmail.com>
2018-05-15 19:33:51 +0200

    init

--- prep1.sh
+++ prep1.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-export PATH=/opt/anaconda/bin:$PATH
 conda create --yes --name ds python=3.5
+source activate ds
+jupyter notebook --generate-config
+echo "c = get_config()" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.port = 8000" >> ~/.jupyter/jupyter_notebook_config.py
--- prep2.sh
+++
@@ -1,8 +0,0 @@
-export PATH=/opt/anaconda/bin:$PATH 
-source activate ds
-jupyter notebook --generate-config
-echo "c = get_config()" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.port = 8000" >> ~/.jupyter/jupyter_notebook_config.py
-
